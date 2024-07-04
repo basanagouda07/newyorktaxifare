1e2e30c140920b3e87e4574107f1cd3c02a82bad
Basanagouda M <145677404+basanagouda07@users.noreply.github.com>
2024-07-04 22:00:39 +0530

    Update app.py

--- app.py
+++ app.py
@@ -11,10 +11,11 @@ st.title("Taxi Fare Predictor")
 passenger_count = st.number_input('Passenger Count', min_value=1, max_value=10, value=1)
 trip_distance = st.number_input('Trip Distance', min_value=0.0, value=1.0)
 
-st.write("Optional Inputs:")
-PULocationID = st.selectbox('PULocationID', options=range(1, 301), index=0)
-transaction_day = st.selectbox('Transaction Day', options=range(1, 32), index=0)
-transaction_hour = st.selectbox('Transaction Hour', options=range(0, 24), index=0)
+
+with st.expander("Optional Inputs"):
+    PULocationID = st.selectbox('PULocationID', options=range(1, 301), index=0)
+    transaction_day = st.selectbox('Transaction Day', options=range(1, 32), index=0)
+    transaction_hour = st.selectbox('Transaction Hour', options=range(0, 24), index=0)
 
 if st.button('Predict'):
     features = np.array([[passenger_count, trip_distance, PULocationID, transaction_day, transaction_hour]])
